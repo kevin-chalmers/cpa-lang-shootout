ae1db68e1d979bbcfe5c410aa49c8fee25ab1c18
Kevin Chalmers <k.chalmers@napier.ac.uk>
2017-08-01 13:58:39 +0100

    Continued work on Erlang code

--- src/erlang/commstime.erl
+++ src/erlang/commstime.erl
@@ -1,50 +1,71 @@
 -module(commstime).
--export([start/0, commstime/1, printer/0, delta/0, succ/0, prefix/1]).
+-export([start/0, commstime/2, printer/1, delta/1, succ/1, prefix/2]).
 
-id(Out) ->
+id(Out, Count) ->
   receive N -> Out ! N end,
-  id(Out).
-
-prefix(N) ->
-  receive Out -> Out ! N end,
-  id(Out).
-
-printer(Count) ->
   if
-    Count rem 10000 == 0 ->
-      {_, Total} = statistics(wall_clock),
-      receive N -> io:format("~p~n", [Total / 10000]) end,
-      printer(1);
-    true ->
-      receive N -> printer(Count + 1) end
+    Count > 0 -> id(Out, Count - 1);
+    true -> ok
   end.
 
-printer() ->
+prefix(N, Count) ->
+  receive Out -> Out ! N end,
+  id(Out, Count).
+
+save_results([], File) ->
+  ok;
+save_results([X|Results], File) ->
+  file:write(File, io_lib:fwrite("~p\n", [X])),
+  save_results(Results, File).
+
+printer(Count, Total, Results) ->
+  if
+    Count rem 10000 == 0 ->
+      {_, Time} = statistics(wall_clock),
+      receive _ ->
+        if
+          Total > 1 -> printer(1, Total - 1, [Time / 10000|Results]);
+          true ->
+            {ok, File} = file:open("ct-erl.csv", [write]),
+            save_results(Results, File)
+        end
+      end;
+    true ->
+      receive _ -> printer(Count + 1, Total - 1, Results) end
+  end.
+
+printer(Total) ->
   statistics(wall_clock),
-  printer(1).
+  printer(1, Total, []).
 
-delta(Out0, Out1) ->
+delta(Out0, Out1, Total) ->
   receive N -> Out0 ! N, Out1 ! N end,
-  delta(Out0, Out1).
+  if
+    Total > 0 -> delta(Out0, Out1, Total - 1);
+    true -> ok
+  end.
 
-delta() ->
-  receive {Out0, Out1} -> delta(Out0, Out1) end.
+delta(Total) ->
+  receive {Out0, Out1} -> delta(Out0, Out1, Total) end.
 
-succ(Out) ->
+succ(Out, Total) ->
   receive N -> Out ! (N + 1) end,
-  succ(Out).
+  if
+    Total > 0 -> succ(Out, Total - 1);
+    true -> ok
+  end.
 
-succ() ->
-  receive Out -> succ(Out) end.
+succ(Total) ->
+  receive Out -> succ(Out, Total) end.
 
-commstime(N) ->
-  Printer = spawn(commstime, printer, []),
-  Delta = spawn(commstime, delta, []),
-  Succ = spawn(commstime, succ, []),
-  Pre = spawn(commstime, prefix, [N]),
-  Delta ! {Printer, Succ},
+commstime(N, Total) ->
+  Delta = spawn(commstime, delta, [Total]),
+  Succ = spawn(commstime, succ, [Total]),
+  Pre = spawn(commstime, prefix, [N, Total]),
+  Delta ! {self(), Succ},
   Pre ! Delta,
-  Succ ! Pre.
+  Succ ! Pre,
+  printer(Total).
 
 start() ->
-  commstime(0).
+  commstime(0, 10000000).
