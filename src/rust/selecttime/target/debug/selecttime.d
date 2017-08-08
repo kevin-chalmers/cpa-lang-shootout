689d4a6e6ea20137ad8b7f4e9469c31be02a4fdb
Kevin Chalmers <k.chalmers@napier.ac.uk>
2017-08-08 21:17:31 +0100

    Select time completed for Rust

---
+++ src/rust/selecttime/target/debug/selecttime.d
@@ -0,0 +1,1 @@
+/home/kevin/cpa-lang-shootout/src/rust/selecttime/target/debug/selecttime: /home/kevin/cpa-lang-shootout/src/rust/selecttime/src/main.rs
---
+++ src/rust/selecttime/target/release/selecttime.d
@@ -0,0 +1,1 @@
+/home/kevin/cpa-lang-shootout/src/rust/selecttime/target/release/selecttime: /home/kevin/cpa-lang-shootout/src/rust/selecttime/src/main.rs
