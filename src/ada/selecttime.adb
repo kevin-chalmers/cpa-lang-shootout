0ff32ba0eabbe3bf61dee9c07d238e4fa4f4c74c
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-08-18 09:16:09 +0100

    Checkpointing before trying new approach for Ada Select Time.

--- src/ada/selecttime.adb
+++ src/ada/selecttime.adb
@@ -11,29 +11,99 @@ use Ada.Real_Time;
 
 procedure SelectTime is
 
-Number_Writers : CONSTANT INTEGER := 1;
+Number_Writers : INTEGER;
+Iterations : CONSTANT INTEGER := 2**24;
+Iterations_Experiment : CONSTANT INTEGER := 2**16;
 
 type Stressed_Packet is record
     Writer : INTEGER;
     N : INTEGER;
 end record;
 
-task type Writer;
-
-task body Writer is
-begin
-    Put_Line("Writer");
-end Writer;
-
-task Reader is
-    entry Send(NATURAL)(Value : in Stressed_Packet);
+task type Reader is
+    entry Send0(Value : in Stressed_Packet);
+    entry Send1(Value : in Stressed_Packet);
+    entry Send2(Value : in Stressed_Packet);
+    entry Send3(Value : in Stressed_Packet);
+    entry Send4(Value : in Stressed_Packet);
+    entry Send5(Value : in Stressed_Packet);
+    entry Send6(Value : in Stressed_Packet);
+    entry Send7(Value : in Stressed_Packet);
 end Reader;
 
 task body Reader is
+    Start : Time;
+    Total : Time_Span;
+    Results : File_Type;
+    i : INTEGER := 0;
 begin
-    Put_Line("Reader");
+    Create(File => Results, Mode => Out_File, Name => "st-ada-" & Integer'Image(Number_Writers) & ".csv");
+    Start := Clock;
+    for count in 0..Iterations loop
+        if count mod 65536 = 0 then
+            Total := (((Clock - Start) / Iterations_Experiment) * 1000000000) / 4;
+            Put_Line(Integer'Image(i) & " " & Float'Image(Float(To_Duration(Total))));
+            Put_Line(Results, Float'Image(Float(To_Duration(Total))));
+            i := i + 1;
+            Start := Clock;
+        end if;
+        select
+            accept Send0(Value : in Stressed_Packet);
+            when Number_Writers > 1 =>
+                accept Send1(Value : in Stressed_Packet);
+            when Number_Writers > 2 =>
+                accept Send2(Value : in Stressed_Packet);
+                accept Send3(Value : in Stressed_Packet);
+            when Number_Writers > 4 =>
+                accept Send4(Value : in Stressed_Packet);
+                accept Send5(Value : in Stressed_Packet);
+                accept Send6(Value : in Stressed_Packet);
+                accept Send7(Value : in Stressed_Packet);
+        end select;
+    end loop;
+    Close(Results);
 end Reader;
+
+task type Writer is
+    entry Construct(ID : in INTEGER; iters : in INTEGER);
+end Writer;
+
+Reader_Task : access Reader := null;
+
+task body Writer is
+    idx : INTEGER;
+    writes : INTEGER;
+    packet : Stressed_Packet;
+begin
+    accept Construct(ID : in INTEGER; iters : in INTEGER) do
+        idx := ID;
+        writes := iters;
+    end;
+    for i in 0..writes loop
+        packet.Writer := idx;
+        packet.N := i;
+        case idx is
+            when 0 => Reader_Task.Send0(packet);
+            when 1 => Reader_Task.Send1(packet);
+            when 2 => Reader_Task.Send2(packet);
+            when 3 => Reader_Task.Send3(packet);
+            when 4 => Reader_Task.Send4(packet);
+            when 5 => Reader_Task.Send5(packet);
+            when 6 => Reader_Task.Send6(packet);
+            when 7 => Reader_Task.Send7(packet);
+            when others => Put_Line("Error");
+        end case;
+    end loop;
+end Writer;
+
+Reader_Task : access Reader := null;
+
+procedure experiment(writers : INTEGER) is
+begin
+    null;
+end experiment;
 
 begin
     Put_Line("Select Time Benchmark");
+    Number_Writers := 1;
 end SelectTime;
--- src/ada/test.adb
+++ src/ada/test.adb
@@ -1,34 +1,55 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Test is
+procedure Multi_Cast is
+    Number_Of_Children : constant Positive := 3;
 
-task type Sender is
-    entry OK;
-end Sender;
+    subtype Children is Positive range 1..Number_Of_Children;
+    type Group is new Natural range 0..Number_Of_Children;
+    type Group_Data_Arrived is array(Group) of Boolean;
 
-task type Receiver is
-    entry Send(T : in Sender);
-end Receiver;
+    protected type Group_Controller is
+        procedure Send(To_Group : in Group; This_Data : in integer);
+        entry Receive(Group)(Data : out integer);
 
-task body Sender is
-begin
-    accept OK do
-        Put_Line("OK");
-    end;
-end Sender;
+    private
+        Arrived : Group_Data_Arrived := (others => false);
+        The_Data : integer;
+    end Group_Controller;
 
-task body Receiver is
-begin
-    accept Send(T : in Sender) do
-        T.OK;
-    end;
-end Receiver;
+    protected body Group_Controller is
 
-S : Sender;
-R : Receiver;
+        procedure Send(To_Group : in Group; This_Data : in integer) is
+        begin
+            if Receive(To_Group)'Count > 0 then
+                Arrived(To_Group) := true;
+                The_Data := This_Data;
+            end if;
+        end Send;
 
-begin
-    R.Send(S);
-    Put_Line("Sent");
-end;
+        entry Receive(for From in Group)(Data : out integer) when Arrived(From) is
+        begin
+            if Receive(From)'Count = 0 then
+                Arrived(From) := false;
+            end if;
+            Data := The_Data;
+        end Receive;
+
+    end Group_Controller;
+
+    Controller_Instance : Group_Controller;
+
+    task type Child_Task is
+        entry Child_Id(Id : in Children);
+    end Child_Task;
+
+    Task_Array : array(Children) of Child_Task;
+
+    task body Child_Task is
+        Local_Data : integer;
+        Local_Id : Children;
+    begin
+        accept Child_Id(Id : in Children) do
+            Local_Id := Id;
+        end Child_Id;
+        
