69ff2a98b406add93d16f4c4e8217d3d1e285f4a
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-09-05 16:29:02 +0100

    Continued work on select time for D.

--- src/d/selecttime.d
+++ src/d/selecttime.d
@@ -1,6 +1,6 @@
 import std.stdio;
 import std.concurrency;
-import std.datetime;
+import std.datetime.stopwatch;
 import core.thread;
 
 struct Packet
@@ -20,5 +20,25 @@ void writer(Tid output, int writer, int count)
 
 void reader(int N, int count)
 {
-    
+    StopWatch sw;
+    Packet p;
+    long[100] results;
+    for (int i = 0; i < count / 65536; ++i)
+    {
+        sw.start();
+        for (int j = 0; j < 65536; ++j)
+        {
+            p = receiveOnly!Packet();
+        }
+        sw.stop();
+        results[i] = sw.peek.total!"nsecs" / 65536;
+        writeln(results[i]);
+        sw.reset();
+    }
+}
+
+void main()
+{
+    Tid reader = spawn(&reader, 0, 65536);
+    Tid writer = spawn(&writer, reader, 0, 65536);
 }
