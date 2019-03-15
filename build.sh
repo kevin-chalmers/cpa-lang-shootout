c3e34519a0beea083823d68e2004c4d8fef4045d
Kevin Chalmers <k.chalmers@napier.ac.uk>
2019-03-15 12:16:29 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ for dir in */ ; do
     if [-e "$dir/Dockerfile"] then
         echo "Building ${dir%*/}"
         docker build -t ${dir%*/} $dir
+    else
+        echo "No Dockerfile for ${dir%*/}"
     fi
 done
