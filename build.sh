a45a4b362a3f7ea34f29549ac2b8956e42e2c426
Kevin Chalmers <k.chalmers@napier.ac.uk>
2019-03-15 12:18:27 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cd src
 for dir in */ ; do
-    if [-e "$dir/Dockerfile"] then
+    if [-e "$dir/Dockerfile"]; then
         echo "Building ${dir%*/}"
         docker build -t ${dir%*/} $dir
     else
